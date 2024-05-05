49e404404d2c610f31296bd1c836a408857e07b0
naokatu <naokatu4@gmail.com>
2024-05-06 03:15:54 +0900

    Initial commit

---
+++ test/hulink-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HulinkInfra from '../lib/hulink-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hulink-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HulinkInfra.HulinkInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
